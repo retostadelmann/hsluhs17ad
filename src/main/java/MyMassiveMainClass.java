4c26c471cbe19fef500a60971bec1c51aae130d4
retostadelmann <reto.stadelmann@ioz.ch>
2017-10-23 17:47:19 +0200

    Aufgabe 2 SW05

--- src/main/java/MyMassiveMainClass.java
+++ src/main/java/MyMassiveMainClass.java
@@ -1,3 +1,4 @@
+import ch.hslu.ad.sw05.AdditionTask;
 import ch.hslu.ad.sw05.Bank;
 import ch.hslu.ad.sw05.BankAccount;
 import org.apache.logging.log4j.LogManager;
@@ -17,16 +18,23 @@ public class MyMassiveMainClass {
     static Logger LOG = LogManager.getLogger(MyMassiveMainClass.class);
 
     public static void main(final String[] args) throws InterruptedException {
-        Bank b = new Bank(400, 200);
-        b.StartTransfer();
-        Thread.sleep(800);
         
-        b.getSourceAccounts().forEach((ba) -> {
-            LOG.info("Balance for source Account " + ba + ": " + ba.getBalance());
-        });
+        AdditionTask at1 = new AdditionTask(4, 9383875);
+        AdditionTask at2 = new AdditionTask(35256, 29383875);
+        AdditionTask at3 = new AdditionTask(26463, 89383875);
         
-        b.getDestAccounts().forEach((ba) -> {
-            LOG.info("Balance for dest Account " + ba + ": " + ba.getBalance());
-        });
+        Thread t1 = new Thread(at1);
+        Thread t2 = new Thread(at2);
+        Thread t3 = new Thread(at3);
+        
+        t1.start();
+        t2.start();
+        t3.start();
+        
+        /*Thread.sleep(20);
+        
+        at1.Stop();
+        at2.Stop();
+        at3.Stop();*/
     }
 }
---
+++ src/main/java/ch/hslu/ad/sw05/AdditionTask.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hslu.ad.sw05;
+
+import java.util.logging.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ *
+ * @author reto.stadelmann
+ */
+public class AdditionTask implements Runnable {
+
+        static Logger LOG = LogManager.getLogger(AdditionTask.class);
+
+    
+    private Thread runThread;
+    private int rangeBegin, rangeEnd;
+    private boolean isStopped = false;
+
+    public AdditionTask(int rangeBegin, int rangeEnd) {
+        this.rangeBegin = rangeBegin;
+        this.rangeEnd = rangeEnd;
+    }
+
+    public void Stop() {
+        this.isStopped = true;
+    }
+
+    public boolean isStopped() {
+        return this.isStopped;
+    }
+
+    @Override
+    public void run() {
+        int n = this.rangeEnd - this.rangeBegin;
+
+        this.runThread = Thread.currentThread();
+// Initialisierungsphase
+        long sum = 0;
+// Arbeitsphase
+        for (int i = this.rangeBegin; i <= this.rangeEnd; i++) {
+            sum += i;
+            /*try {
+                Thread.sleep(1);
+            } catch (InterruptedException ex) {
+                java.util.logging.Logger.getLogger(AdditionTask.class.getName()).log(Level.SEVERE, null, ex);
+            }*/
+        }
+// Aufräumphase
+        if (!isStopped()) {
+            LOG.info(runThread.getName() + ": SUM" + n + " -> " + sum);
+        } else {
+            LOG.info(runThread.getName() + ": interrupted.");
+        }
+    }
+}
